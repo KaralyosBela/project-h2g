61c05727b1985bcde0b644573a53781bba14574e
Karalyos Bela <karalyosbela@gmail.com>
2022-06-16 14:02:56 +0200

    toolbar done with css

--- react-app/src/components/MainNavigation.js
+++ react-app/src/components/MainNavigation.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
   return (
-    <header>
-      <div>App</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>Weather application</div>
       <nav>
-        <ul>
+        <ul className={classes.ul}>
           <li>
             <Link to="/">Home</Link>
           </li>
