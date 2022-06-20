7d04a8998d2388406576a722d952fa92ed45741c
Karalyos Bela <karalyosbela@gmail.com>
2022-06-20 11:50:26 +0200

    typescript atiras mukodik

--- react-app/src/components/DailyWeather.tsx
+++ react-app/src/components/DailyWeather.tsx
@@ -1,21 +1,26 @@
 import Card from './Card';
 import Weather from './Weather.interface'
 
-const DailyWeatherTS: React.FC<Weather> = ({temperature, city, icon}) => {
+type Props = {
+    weatherInfo: Weather
+}
+// const DailyWeather: React.FC<Props> = ({num}) => {
+
+ const DailyWeather: React.FC<Props> = ({weatherInfo}) => {
     
     return <div>
         <Card day="Today">
         <div>
         <div>
-        <img src = {`http://openweathermap.org/img/wn/${icon}.png`} alt="hehe"/>
+        <img src = {`http://openweathermap.org/img/wn/${weatherInfo.icon}.png`} alt="hehe"/>
         </div>
         <div>
-            <div><span>Temp</span> {temperature}</div>
-            <div><span>City</span> {city}</div>
+            <div><span>Temp</span> {weatherInfo.temperature}</div>
+            <div><span>City</span> {weatherInfo.city}</div>
         </div>
         </div>
         </Card>
     </div>
 }
 
-export default DailyWeatherTS;
+export default DailyWeather;
--- react-app/src/pages/WeatherPage.tsx
+++ react-app/src/pages/WeatherPage.tsx
@@ -4,7 +4,7 @@ import Container from "../components/Container";
 import DailyWeather from "../components/DailyWeather";
 import Weather from '../components/Weather.interface';
 
-const WeatherPageTS: React.FC = () => {
+const WeatherPage: React.FC = () => {
   const [weather, setWeather] = useState<Weather | null>(null);
 
   useEffect(() => {
@@ -13,7 +13,7 @@ const WeatherPageTS: React.FC = () => {
         const response = await axios.get(
           `https://api.openweathermap.org/data/2.5/weather?lat=30.44&lon=-94.04&appid=${process.env.REACT_APP_API_KEY}&unit=metric`
         );
-        
+
         setWeather({
           temperature: response.data.main.temp,
           city: response.data.name,
@@ -26,6 +26,7 @@ const WeatherPageTS: React.FC = () => {
         console.log(error);
       }
     };
+
     fetchWeatherApi();
     //Csak mount-nal akarunk fetchelni
   }, []);
@@ -33,11 +34,15 @@ const WeatherPageTS: React.FC = () => {
   return (
     <div>
       <Container>
+      {/* <div className={classes.searchBar}>
+        <input type="text" ref={cityInputRef}></input>
+        <button onClick={clicked}>Search city</button>
+        </div> */}
         <h1>Todays weather in [ ]</h1>
-        {!!weather && <DailyWeather weatherInfo={weather} />}
+        {!!weather && <DailyWeather weatherInfo={weather}/>}
       </Container>
     </div>
   );
 };
 
-export default WeatherPageTS;
+export default WeatherPage;
