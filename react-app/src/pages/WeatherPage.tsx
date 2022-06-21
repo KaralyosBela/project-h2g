877f5429c6ae9ccf768dbeaa44492d2988676090
Karalyos Bela <karalyosbela@gmail.com>
2022-06-21 12:05:15 +0200

    errors fixed

--- react-app/src/pages/WeatherPage.tsx
+++ react-app/src/pages/WeatherPage.tsx
@@ -2,26 +2,57 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Container from "../components/Container";
 import DailyWeather from "../components/DailyWeather";
-import {Weather} from "../interfaces/Weather.interface";
+import { Weather } from "../interfaces/Weather.interface";
 import "../styles/Custom.module.css";
+
+interface ICity {
+  country: string,
+  lat: number,
+  lon: number,
+  city: string
+}
 
 const WeatherPage: React.FC = () => {
   const [weather, setWeather] = useState<Weather | null>(null);
+  const [cityData, setCityData] = useState<ICity | null>(null);
+
+  const searchCity = async () => {
+    const cityName = "Debrecen";
+    const response = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}`
+    );
+    const data = {
+      country: response.data[0].country,
+      lat: response.data[0].lat,
+      lon: response.data[0].lon,
+      city: response.data[0].name,
+    };
+    setCityData(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchCity();
+  },[])
 
   useEffect(() => {
     const fetchWeatherApi = async () => {
       try {
+        console.log(cityData)
+        if(cityData) {
         const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=30.44&lon=-94.04&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+          //Itt miért lett autcomplete a ?
+          `https://api.openweathermap.org/data/2.5/weather?lat=${cityData?.lat}&lon=${cityData?.lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
         );
-
+        
+        //use state async
         setWeather({
           temperature: response.data.main.temp,
           city: response.data.name,
           icon: response.data.weather[0].icon,
         });
-
-        //miért nem íródik ki
+      }
+        //miért nem íródik ki itt még
         console.log(weather);
       } catch (error) {
         console.log(error);
@@ -30,15 +61,15 @@ const WeatherPage: React.FC = () => {
 
     fetchWeatherApi();
     //Csak mount-nal akarunk fetchelni
-  }, []);
+  }, [cityData]);
 
   return (
     <Container>
       <div>
         <input type="text"></input>
-        <button>Search city</button>
+        <button onClick={searchCity}>Search city</button>
       </div>
-      <h1>Todays weather in [ ]</h1>
+      <h1>Todays weather in [{cityData?.city}]</h1>
       {!!weather && <DailyWeather weatherInfo={weather} />}
     </Container>
   );
--- react-app/src/pages/WeeklyWeatherPage.tsx
+++ react-app/src/pages/WeeklyWeatherPage.tsx
@@ -6,7 +6,7 @@ import "../styles/Custom.module.css";
 import { IWeeklyWeather } from "../interfaces/Weather.interface";
 
 const WeeklyWeatherPage: React.FC = () => {
-  const [dailyWeather, setDailyWeather] = useState<IWeeklyWeather[] | null>([]);
+  const [dailyWeather, setDailyWeather] = useState<IWeeklyWeather[]>([]);
 
   useEffect(() => {
     const fetchWeatherApi = async () => {
@@ -15,7 +15,8 @@ const WeeklyWeatherPage: React.FC = () => {
           `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=${process.env.REACT_APP_API_KEY}&units=metric`
         );
 
-        //TODO: itt mit kéne
+        console.log(response.data.daily);
+        //TODO: az apinak kellene interface, és az lenne a type az itemnek
         const asd = response.data.daily.map((item: any) => {
           return {
             temperature: item.temp.day,
@@ -31,6 +32,10 @@ const WeeklyWeatherPage: React.FC = () => {
     };
     fetchWeatherApi();
   }, []);
+
+  // miért lesz ez null elsőnek
+  // console.log(dailyWeather);
+
 
   return (
     <Container>
