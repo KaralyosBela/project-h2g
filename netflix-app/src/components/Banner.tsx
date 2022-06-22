bedae3211d81c464fdb2683538e3e565a5eee4d2
Karalyos Bela <karalyosbela@gmail.com>
2022-06-22 14:23:43 +0200

    banner and footer css done

--- netflix-app/src/components/Banner.tsx
+++ netflix-app/src/components/Banner.tsx
@@ -2,8 +2,24 @@ import classes from "./Banner.module.css";
 
 export const Banner: React.FC = () => {
   return (
-    <div className={classes.banner}>
- 
+    <div className={classes.picture}>
+      <div className={classes.banner}>
+        <div className={classes.upperBar}>
+          <div>
+            <h4 className={classes.upperBarTitle}>
+              <span>netflix</span>Roulette
+            </h4>
+          </div>
+          <button className={classes.addButton}>+ ADD MOVIE</button>
+        </div>
+        <div className={classes.title}>
+          <h1>FIND YOUR MOVIE</h1>
+        </div>
+        <div className={classes.searchBar}>
+          <input placeholder="What do you want to watch?"></input>
+          <button className={classes.searchbutton}>SEARCH</button>
+        </div>
+      </div>
     </div>
   );
 };
--- netflix-app/src/components/Footer.tsx
+++ netflix-app/src/components/Footer.tsx
@@ -2,6 +2,6 @@ import classes from "./Footer.module.css";
 
 export const Footer: React.FC = () => {
   return <div className={classes.footer}>
-    <h5><span>netflix</span>roulette</h5>
+    <h4><span>netflix</span>roulette</h4>
   </div>;
 };
