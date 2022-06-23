11356fbc1ff70e4e50e8af230ceeff8ed52bbb80
Karalyos Bela <karalyosbela@gmail.com>
2022-06-23 12:40:03 +0200

    movies json implemented, css fixes

---
+++ netflix-app/src/interfaces/movies.interface.ts
@@ -0,0 +1,9 @@
+export interface IMovies {
+  title: string;
+  release_date: string;
+  genre: string;
+  thumbnail: string;
+  movie_url: string;
+  rating: string;
+  runtime: string;
+}
--- netflix-app/src/pages/HomePage.tsx
+++ netflix-app/src/pages/HomePage.tsx
@@ -1,28 +1,57 @@
+import { useEffect, useState } from "react";
 import { Banner } from "../components/Banner";
 import { FilterBar } from "../components/FilterBar";
 import { Footer } from "../components/Footer";
-import { Movie } from "../components/Movie";
 import { MovieList } from "../components/MovieList";
 import { Layout } from "../ui/Layout";
+import { IMovies } from "../interfaces/movies.interface";
 
 export const HomePage: React.FC = () => {
+const [movies, setMovies] = useState<IMovies[]>([])
+const [moviesCount, setMoviesCount] = useState<number>(0);
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch("movies.json", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      const data = await response.json();
+   
+      const movieData = data.movies.map((item: IMovies) => {
+        return {
+            title: item.title,
+            release_date: item.release_date,
+            genre: item.genre,
+            thumbnail: item.thumbnail,
+            movie_url: item.movie_url,
+            rating: item.rating,
+            runtime: item.runtime
+        };
+      });
+
+      setMoviesCount(movieData.length);
+      setMovies(movieData);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
       <Layout>
         <Banner />
-        <FilterBar/>
-          <MovieList>
-            <Movie/>
-            <Movie/>
-            <Movie/>
-            <Movie/>
-            <Movie/>
-            <Movie/>
-            <Movie/>
-            <Movie/>
-            <Movie/>
-          </MovieList>
-        <Footer/>
+        <FilterBar movieCount={moviesCount}/>
+        <MovieList moviesList={movies}>
+        </MovieList>
+        <Footer />
       </Layout>
     </div>
   );
